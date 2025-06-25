3770478e09f7782f99ed5ea9121c71bc5705de78
Ana Clara Barros Souza <clara.barros@compass.uol>
2025-06-25 11:18:57 -0300

    config: spring initializr

---
+++ backend/hackathon-AI/src/main/java/com/hackathon_AI/HackathonAiApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon_AI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonAiApplication.class, args);
+	}
+
+}
